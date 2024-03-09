f2aa7b1f9c608173cfb0dd72e4ec879b2d539eef
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-03-09 19:37:01 +0100

    Add scores and away teams

--- Scraper.cs
+++ Scraper.cs
@@ -9,13 +9,26 @@ static public class Scraper
 
         var title = document.DocumentNode.SelectNodes("//*[@id=\"content\"]/h1").First().InnerText;
         var awayTeams = document.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[1]/td[1]/a");
-        
-        Console.WriteLine(title);
+        var homeTeams = document.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[2]/td[1]/a");
+        var homeTeamScores = document.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[1]/td[2]");
+        var awayTeamScores =  document.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[2]/td[2]");
 
-        if (awayTeams != null)
+        Console.WriteLine(title);
+        if (awayTeamScores != null)
         {
-            // Print each team name
-            foreach (var teamNode in awayTeams)
+            foreach (var teamNode in homeTeamScores)
+            {
+                Console.WriteLine(teamNode.InnerText.Trim());
+            }
+        }
+        else
+        {
+            Console.WriteLine("No away teams found.");
+        }
+
+        if (homeTeamScores != null)
+        {
+            foreach (var teamNode in homeTeamScores)
             {
                 Console.WriteLine(teamNode.InnerText.Trim());
             }
@@ -27,12 +40,23 @@ static public class Scraper
     }
 }
 
-// Away:
+// Away teams:
 //*[@id=\"content\"\]/div[3]/div/table[1]/tbody/tr[1]/td[1]/a
 //*[@id=\"content\"]/div[3]/div[2]/table[1]/tbody/tr[1]/td[1]/a  
 //*[@id=\"content\"]/div[3]/div[3]/table[1]/tbody/tr[1]/td[1]/a
 
-// Home:
+// Home teams:
 //*[@id=\"content\"]/div[3]/div[1]/table[1]/tbody/tr[2]/td[1]/a 
 //*[@id=\"content\"]/div[3]/div[2]/table[1]/tbody/tr[2]/td[1]/a
 //*[@id=\"content\"]/div[3]/div[3]/table[1]/tbody/tr[2]/td[1]/a
+
+// Away scores:
+// *[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[1]/td[2]
+//*[@id=\"content\"]/div[3]/div[2]/table[1]/tbody/tr[1]/td[2]
+//*[@id="content"]/div[3]/div[4]/table[1]/tbody/tr[1]/td[2]
+
+//*[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[1]/td[2]
+
+// Home scores:
+//*[@id=\"content\"]/div[3]/div/table[1]/tbody/tr[2]/td[2]
+//*[@id="content"]/div[3]/div[2]/table[1]/tbody/tr[2]/td[2]
